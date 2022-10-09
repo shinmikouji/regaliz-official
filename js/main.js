483400db2e8f2444042e5d0d38f284241f48d80c
regaliz-develop <sales.regaliz@gmail.com>
2022-10-09 23:36:44 +0900

    page-top etc

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 jQuery(function ($) {
+  const pageTop = $('#page-top');
   $(window).scroll(function () {
     const scroll	= $(this).scrollTop();
-    console.log(scroll)
+    console.log(scroll);
     if(scroll > 90) {
       $(".l-header").addClass('l-header--borderBottom');
+      pageTop.fadeIn(500);
     } else {
       $(".l-header").removeClass('l-header--borderBottom');
+      pageTop.fadeOut(500);
     }
+  })
+  pageTop.click(function () {
+    $('body, html').animate({scrollTop: 0}, 700);
+    return false;
   })
 });
